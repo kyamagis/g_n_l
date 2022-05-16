06dfe43d90f3e5019e881fd7027e1837b5c17ce0
Kakuya Yamagishi <kyamagis@c1r17s9.42tokyo.jp>
2022-05-16 19:10:12 +0900

    4th

--- get_next_line.h
+++ get_next_line.h
@@ -6,56 +6,26 @@
 /*   By: kyamagis <kyamagis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 11:04:28 by kyamagis          #+#    #+#             */
-/*   Updated: 2022/05/14 16:27:52 by kyamagis         ###   ########.fr       */
+/*   Updated: 2022/05/16 16:28:38 by kyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdio.h>
-# include <stdint.h>
+# define _POSIX_SOURCE
 # include <stdlib.h>
-# include <string.h>
+# include <stdio.h>
+# include <limits.h>
 # include <unistd.h>
+# include <string.h>
+# include <fcntl.h>
+# include <stdint.h>
 
-# define FD_CB_NUM 5
-# define BUFFER_SIZE 10
-
-typedef struct t_data_buff
-{
-	char	*saved;
-	char	*prefix;
-	char	*buff;
-	int		readsz;
-	int		newl_flag;
-	int		null_flg;
-}	t_data_buff;
-
-typedef struct t_fd_cb
-{
-	int			flg;
-	int			fd;
-	t_data_buff	*pt;
-}	t_fd_cb;
-
-char		*get_next_line(int fd);
-void	*ft_join_to_nl(t_data_buff *outp_line);
-int			ft_strchr_idx(char *s, int c);
-char		*ft_strdup(char *s1);
-size_t		ft_strlcpy(char *dest, char *src, size_t size);
-size_t		ft_strlen(char *s);
+char	*get_next_line(int fd);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+size_t	ft_strlcpy(char *dest, char *src, size_t size);
+char	*ft_free_str(char *str);
+char	*ft_strdup(char *s1);
 #endif
-/*
-typedef struct t_data_buff
-{
-	char	*joined;
-	int		nul_flag;
-	int		readsz;
-	char	*buff;
-}	t_data_buff;
-
-outp_line->buff = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1)); // 同じ意味？
-	if (outp_line->buff == NULL)
-		return (NULL);
-*/
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,63 +6,42 @@
 /*   By: kyamagis <kyamagis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 16:47:41 by kyamagis          #+#    #+#             */
-/*   Updated: 2022/05/14 17:11:36 by kyamagis         ###   ########.fr       */
+/*   Updated: 2022/05/16 16:24:30 by kyamagis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen_to_nl (t_data_buff *outp_line)
+char	*ft_strchr(char *s, int c)
 {
-	size_t len_bf;
+	size_t			i;
+	unsigned char	*suc;
+	unsigned char	cuc;
 
-	len_bf = 0;
-	while (outp_line->buff[len_bf] != '\0' && outp_line->buff[len_bf] != '\n')
-		len_bf++;
-	if (outp_line->buff[len_bf] == '\n')
-	{
-		outp_line->newl_flag = 1;
-		len_bf++;
-		outp_line->saved = ft_strdup(&outp_line->buff[len_bf]);
-		if (outp_line->saved == NULL)
-			outp_line->null_flg = 1;
-	}
-	return (len_bf);
-}
-
-void	*ft_join_to_nl(t_data_buff *outp_line)
-{
-	char	*joined;
-	size_t	len_pr;
-	size_t	len_bf;
-
-	len_pr = ft_strlen(outp_line->prefix);
-	len_bf = ft_strlen_to_nl(outp_line);
-	if (outp_line->newl_flag == 1)
-		return;
-	joined = (char *)malloc(sizeof(char) * (len_pr + len_bf + 1));
-	if (joined == NULL)
-	{
-		outp_line->null_flg = 1;
-		return;
-	}
-	ft_strlcpy(joined, outp_line->prefix, len_pr + 1);
-	ft_strlcpy(&joined[len_pr], outp_line->buff, len_bf + 1);
-	ft_free_str(outp_line);//free(buff);buff == NULL いる？
-	outp_line->prefix = joined;
-}
-
-char	*ft_strdup(char *s1)
-{
-	char	*prc;
-	size_t	lens1;
-
-	lens1 = ft_strlen(s1);
-	prc = (char *)malloc(sizeof(char) * (lens1 + 1));
-	if (prc == NULL)
+	if (s == NULL)
 		return (NULL);
-	ft_strlcpy(prc, s1, lens1 + 1);
-	return (prc);
+	suc = (unsigned char *)s;
+	cuc = (unsigned char)c;
+	i = 0;
+	while (suc[i] != '\0')
+	{
+		if (suc[i] == cuc)
+			return ((char *)suc + i);
+		i++;
+	}
+	if (cuc == '\0')
+		return ((char *)suc + i);
+	return (NULL);
+}
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
 
 size_t	ft_strlcpy(char *dest, char *src, size_t size)
@@ -71,6 +50,11 @@ size_t	ft_strlcpy(char *dest, char *src, size_t size)
 	size_t	lensrc;
 	char	*castsrc;
 
+	if (src == NULL)
+	{
+		dest[0] = '\0';
+		return (0);
+	}
 	castsrc = (char *) src;
 	lensrc = ft_strlen(src);
 	if (size == 0)
@@ -85,30 +69,51 @@ size_t	ft_strlcpy(char *dest, char *src, size_t size)
 	return (lensrc);
 }
 
-size_t	ft_strlen(char *s)
+char	*ft_free_str(char *str)
 {
-	size_t	i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
+	free(str);
+	str = NULL;
+	return (NULL);
 }
 
-int	main(void)
+char	*ft_strdup(char *s1)
+{
+	char	*prc;
+	size_t	lens1;
+
+	lens1 = 0;
+	if (s1 != NULL)
+		lens1 = ft_strlen(s1);
+	prc = (char *)malloc(sizeof(char) * (lens1 + 1));
+	if (prc == NULL)
+		return (NULL);
+	ft_strlcpy(prc, s1, lens1 + 1);
+	return (prc);
+}
+
+/*int	main(void)
 {
 	char *str;
+	//char *str1;
 	t_data_buff	*outp_1;
 
-	// outp_1->prefix = ;
-	// outp_1->buff = ;
-	// outp_1->saved = ;
 	outp_1 = (t_data_buff *)malloc(sizeof(t_data_buff));
+	outp_1->prefix = (char *)malloc(sizeof(char) * (4 + 1));
+	outp_1->prefix = "GET_";
+	outp_1->buff = (char *)malloc(sizeof(char) * (5 + 1));
+	outp_1->buff = "WILD\n";
+	ft_join_to_nl(outp_1);
 
-	ft_str_nl (outp_1, 0);
-	printf("%d", outp_1->null_flg);
+	// ft_str_nl (outp_1, 0);
+	// printf("%d\n", outp_1->null_flg);
 	
+	printf("prefix%s\n", outp_1->prefix);
+	printf("buff  %s\n", outp_1->buff);
+	printf("saved %s\n", outp_1->saved);
+	printf("nl_flg%d\n", outp_1->newl_flag);
+	
+		printf("dup%s\n", ft_strdup(NULL));
 	str = (char *)malloc(sizeof(char) * (5));
-	str[1] = '1';
-	printf("%s", str);
-}
+	str[0] = '1';
+	printf("%s\n", str);
+}*/
